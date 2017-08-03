af0635bca468d394b4a91e6932694bbbe0184882
ALMEIDA <skyknight1989@gmail.com>
2017-08-02 21:18:17 -0300

    Image Picker almost implemente.

--- CarouselApp/CarouselApp/PhotoSelectorController.swift
+++ CarouselApp/CarouselApp/PhotoSelectorController.swift
@@ -10,10 +10,74 @@ import UIKit
 
 class PhotoSelectorController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
-    
+    let headerId = "headerId"
+    let cellId = "cellId"
     
     override func viewDidLoad() {
-        collectionView?.backgroundColor = .red
+        collectionView?.backgroundColor = .white
+        
+        collectionView?.register(HeaderPhotoSelectorCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+        
+        collectionView?.register(PhotoSelectorCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let screenWidth = view.frame.width
+        
+        let cellWidth = (screenWidth - 2)/3
+        
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId, for: indexPath) as! HeaderPhotoSelectorCell
+        
+        header.backgroundColor = .yellow
+        
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        let side = view.frame.width
+        
+        return CGSize(width: side, height: side)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PhotoSelectorCell
+        
+        let r = randomNumber(max: 255)
+        let g = randomNumber(max: 255)
+        let b = randomNumber(max: 255)
+        
+        cell.backgroundColor = UIColor.rgb(red: r, green: g, blue: b)
+        
+        return cell
+    }
+    
+    func randomNumber(max: Int) -> CGFloat{
+        let random = arc4random_uniform(UInt32(max))
+        
+        return CGFloat(random)
     }
     
     
